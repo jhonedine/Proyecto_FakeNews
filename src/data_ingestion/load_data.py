f8f8ffb76f639fe358e546a49dd5dc622e6c483b
jhonedine <127271785+jhonedine@users.noreply.github.com>
2025-07-03 22:52:34 -0500

    Update load_data.py
    update codigo

--- src/data_ingestion/load_data.py
+++ src/data_ingestion/load_data.py
@@ -1,6 +1,14 @@
 
 import pandas as pd
 
+from google.colab import drive
+drive.mount('/content/drive')
+fake = ('https://drive.google.com/uc?id=111J3U3feYR0ri6_DlP79CGRL-WsmLiF3')
+true = ('https://drive.google.com/uc?id=18-_Mg6osJGrLmW3baJTgW_-LWcPCopCE')
+df_fake = pd.read_csv(fake, sep=None, engine='python')
+df_true = pd.read_csv(true, sep=None, engine='python')
+
+
 def load_fake_news_data():
     df_fake = pd.read_csv("data/Fake.csv")
     df_real = pd.read_csv("data/True.csv")
